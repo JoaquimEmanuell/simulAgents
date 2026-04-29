0dd8847f9b50a55d4b12772e3374e42cbf4e03ec
joaquimgamer23@gmail.com <joaquimgamer23@gmail.com>
2026-04-29 15:32:25 -0300

    added gitignore

--- test_api.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key="AIzaSyADKhqHD-ofKyh1mGXTANih4KD9ocq4fXw")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
